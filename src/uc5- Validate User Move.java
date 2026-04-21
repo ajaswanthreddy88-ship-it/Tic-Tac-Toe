ba7992d654654d068268f5ce888057fd168cf9c8
jaswanth <ajaswanthreedy88@gmail.com>
2026-04-21 13:28:33 +0530

    Initial

---
+++ src/uc5- Validate User Move.java
@@ -0,0 +1,13 @@
+static boolean isValidMove(int row, int col) {
+    // Check boundaries
+    if (row < 0 || row > 2 || col < 0 || col > 2) {
+        return false;
+    }
+
+    // Check if cell is empty
+    if (board[row][col] != '-') {
+        return false;
+    }
+
+    return true;
+}
